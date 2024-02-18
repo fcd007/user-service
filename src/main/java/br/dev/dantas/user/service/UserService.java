7336069e722225e9ceab86e2a5ae09acbc00b69b
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-18 14:46:11 -0300

    feat: create and add user service

--- src/main/java/br/dev/dantas/user/service/UserService.java
+++ src/main/java/br/dev/dantas/user/service/UserService.java
@@ -1,2 +1,18 @@
-package br.dev.dantas.user.service;public class UserService {
+package br.dev.dantas.user.service;
+
+import br.dev.dantas.user.domain.entity.User;
+import br.dev.dantas.user.repository.config.UserHardCodeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserHardCodeRepository userHardCodeRepository;
+    public List<User> listAll() {
+        return userHardCodeRepository.findAll();
+    }
 }
