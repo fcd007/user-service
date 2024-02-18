bec85b2e20bc170f1eaf83cfc18918b03ccfcc7c
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-18 14:45:12 -0300

    feat: create user data reposotory

--- src/main/java/br/dev/dantas/user/domain/mappers/UserMapper.java
+++ src/main/java/br/dev/dantas/user/domain/mappers/UserMapper.java
@@ -1,2 +1,19 @@
-package br.dev.dantas.user.domain.mappers;public class UserMapper {
+package br.dev.dantas.user.domain.mappers;
+
+import br.dev.dantas.user.controller.request.UserPostRequest;
+import br.dev.dantas.user.controller.response.UserGetResponse;
+import br.dev.dantas.user.domain.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+
+import java.util.List;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface UserMapper {
+
+    @Mapping(target = "id", expression = "java(java.util.concurrent.ThreadLocalRandom.current().nextLong(100_000))")
+    User toUser(UserPostRequest request);
+
+    List<UserGetResponse> toUserGetResponseList(List<User> users);
 }
