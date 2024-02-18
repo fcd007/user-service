bf68b75c85159d91def13d3245f97212536394f5
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-18 14:45:42 -0300

    feat: add repository

--- src/main/java/br/dev/dantas/user/repository/config/UserData.java
+++ src/main/java/br/dev/dantas/user/repository/config/UserData.java
@@ -1,2 +1,25 @@
-package br.dev.dantas.user.repository.config;public class UserData {
+package br.dev.dantas.user.repository.config;
+
+import br.dev.dantas.user.domain.entity.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserData {
+    private List<User> users = new ArrayList<>();
+
+    {
+        var adele = User.builder().id(1L).firstName("Adele").lastName("Dinner").email("adele@gmail.com").build();
+        var celine = User.builder().id(2L).firstName("Celine").lastName("Dion").email("celine@gmail.com").build();
+        var laura = User.builder().id(3L).firstName("Laura").lastName("Pausini").email("laura@gmail.com").build();
+
+        users.addAll(List.of(adele, celine, laura));
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
 }
--- src/main/java/br/dev/dantas/user/repository/config/UserHardCodeRepository.java
+++ src/main/java/br/dev/dantas/user/repository/config/UserHardCodeRepository.java
@@ -1,2 +1,27 @@
-package br.dev.dantas.user.repository.config;public class UserHardCodeRepository {
+package br.dev.dantas.user.repository.config;
+
+import br.dev.dantas.user.domain.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserHardCodeRepository {
+
+    private final UserData userData;
+
+    public List<User> findAll() {
+        return userData.getUsers();
+    }
+
+    public Optional<User> findById(Long id) {
+        return userData.getUsers().stream().filter(user -> user.getId().equals(id)).findFirst();
+    }
+
+    public List<User> findByName(String name) {
+        return name == null ? userData.getUsers() : userData.getUsers().stream().filter(user -> user.getFirstName().equalsIgnoreCase(name)).toList();
+    }
 }
