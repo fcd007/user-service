e7d66bd43a59a559919e86cfd497b7669cf6c1be
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-18 14:44:05 -0300

    feat: create controller rest user

--- src/main/java/br/dev/dantas/user/controller/IUserController.java
+++ src/main/java/br/dev/dantas/user/controller/IUserController.java
@@ -1,4 +1,8 @@
-package br.dev.dantas.user.controller.usercontroller;
+package br.dev.dantas.user.controller;
 
 public interface IUserController {
+
+    public static final String V1_PATH_DEFAULT = "/api/v1/users";
+
+    public static final String V1_PATH_OTHER = "/api/v1/users/";
 }
--- src/main/java/br/dev/dantas/user/controller/UserController.java
+++ src/main/java/br/dev/dantas/user/controller/UserController.java
@@ -1,8 +1,35 @@
-package br.dev.dantas.user.controller.usercontroller;
+package br.dev.dantas.user.controller;
 
+
+import br.dev.dantas.user.controller.response.UserGetResponse;
+import br.dev.dantas.user.domain.mappers.UserMapper;
+import br.dev.dantas.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = {IUserController.V1_PATH_DEFAULT, IUserController.V1_PATH_OTHER})
+@Log4j2
+@RequiredArgsConstructor
 public class UserController {
 
-    public static final String V1_PATH_DEFAULT = "/api/v1/users";
+    private final UserMapper userMapper;
+    private final UserService userService;
 
-    public static final String V1_PATH_OTHER = "/api/v1/users/";
+    @GetMapping
+    public ResponseEntity<List<UserGetResponse>> list() {
+        log.info("Request received to list all users");
+
+        var user = userService.listAll();
+        var userGetResponse = userMapper.toUserGetResponseList(user);
+
+        return ResponseEntity.ok(userGetResponse);
+    }
 }
