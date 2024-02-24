07290c25fa65aa2f1c9b81c5af085aedbbdc94c6
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-24 17:57:14 -0300

    test:  user service

--- src/main/java/br/dev/dantas/user/service/UserService.java
+++ src/main/java/br/dev/dantas/user/service/UserService.java
@@ -3,7 +3,9 @@ package br.dev.dantas.user.service;
 import br.dev.dantas.user.domain.entity.User;
 import br.dev.dantas.user.repository.config.UserHardCodeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -14,5 +16,9 @@ public class UserService {
     private final UserHardCodeRepository userHardCodeRepository;
     public List<User> findAll() {
         return userHardCodeRepository.findAll();
+    }
+
+    public User findById(Long id) {
+        return userHardCodeRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
     }
 }
