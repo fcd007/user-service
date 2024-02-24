46901067c0d2b2c8f195bd6cce6846f355f9d6bc
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-02-24 17:55:39 -0300

    test: add user test service

--- src/test/java/br/dev/dantas/user/service/UserServiceTest.java
+++ src/test/java/br/dev/dantas/user/service/UserServiceTest.java
@@ -10,8 +10,10 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -41,6 +43,30 @@ class UserServiceTest {
 
         var users = service.findAll();
         Assertions.assertThat(users).hasSameElementsAs(this.users);
+    }
+
+    @Test
+    @DisplayName("findById() returns a optional user is id exists")
+    @Order(2)
+    void findById_ReturnsOptionalUser_WhenIsIdExists() {
+        var id = 1L;
+        var animeExpected = this.users.get(0);
+        BDDMockito.when(repository.findById(id)).thenReturn(Optional.of(animeExpected));
+
+        var userOptional = service.findById(id);
+        Assertions.assertThat(userOptional).isEqualTo(animeExpected);
+    }
+
+    @Test
+    @DisplayName("findById() returns a throw ResponseStatusException not found")
+    @Order(3)
+    void findById_ThrowResponseStatusException_WhenNoUserIsFound() {
+        var id = 1L;
+        BDDMockito.when(repository.findById(id)).thenReturn(Optional.empty());
+
+        Assertions.assertThatException()
+                .isThrownBy(() -> service.findById(id))
+                .isInstanceOf(ResponseStatusException.class);
     }
 
 }
