c7cf4ea3e84e27ac3ba4e62c3453903c1355de8e
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-03-06 21:42:28 -0300

    feat: adicionado validation pom

--- src/main/java/br/dev/dantas/user/controller/request/UserPostRequest.java
+++ src/main/java/br/dev/dantas/user/controller/request/UserPostRequest.java
@@ -1,6 +1,7 @@
 package br.dev.dantas.user.controller.request;
 
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,9 +11,12 @@ import lombok.ToString;
 @ToString
 public class UserPostRequest {
 
+    @NotBlank(message = "he field firstName is required")
     private String firstName;
 
+    @NotBlank(message = "he field lastName is required")
     private String lastName;
 
+    @NotBlank(message = "he field email is required")
     private String email;
 }
