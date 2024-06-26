1dd72089f94e68b50c0e70a769d49406e1102449
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-06-26 09:17:02 -0300

    test(ajuste de teste user repositorio): ajuste de teste repositorio

--- src/test/java/br/dev/dantas/user/repository/config/UserRepositoryTest.java
+++ src/test/java/br/dev/dantas/user/repository/config/UserRepositoryTest.java
@@ -2,13 +2,22 @@ package br.dev.dantas.user.repository.config;
 
 import br.dev.dantas.user.commons.UserUtils;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 @Import({UserUtils.class})
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserRepositoryTest {
 
   @Autowired
@@ -22,6 +31,15 @@ class UserRepositoryTest {
     var userToSave = userUtils.newUserToSave();
     var user = userRepository.save(userToSave);
     Assertions.assertThat(user).hasNoNullFieldsOrProperties();
+  }
+
+  @Test
+  @DisplayName("findAll() returns a list with all users")
+  @Order(1)
+  @Sql("/sql/user_init_test.sql")
+  void findAll_ReturnsAllUsers_WhenSuccessful() {
+    var users = userRepository.findAll();
+    Assertions.assertThat(users).isNotEmpty();
   }
 
 }
