957f4b296f228a22815ba495f45dfa880780ec74
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-06-27 23:01:49 -0300

    feat(ajuste de repotitory consultas): consultas personalizadas

--- src/main/java/br/dev/dantas/user/repository/config/UserProfileRepository.java
+++ src/main/java/br/dev/dantas/user/repository/config/UserProfileRepository.java
@@ -1,10 +1,18 @@
 package br.dev.dantas.user.repository.config;
 
 import br.dev.dantas.user.domain.entity.UserProfile;
+import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
+  @Query("SELECT up FROM UserProfile up join fetch up.user u join fetch up.profile p")
+  List<UserProfile> retrieveAll();
+
+  @EntityGraph(attributePaths = {"user", "profile"})
+  List<UserProfile> findAll();
 }
