86dc652462dd59163f5ec4f2caa12ba643574849
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2024-07-29 22:04:29 -0300

    feat(spring security config): ajuste de spring security basic

--- src/main/java/br/dev/dantas/user/config/SecurityConfig.java
+++ src/main/java/br/dev/dantas/user/config/SecurityConfig.java
@@ -2,24 +2,44 @@ package br.dev.dantas.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  private static final String[] WHITE_LIST = {"/swagger-ui/index.html", "/v3/api-docs/**", "/swagger-ui/**" };
+  private static final String PASSWORD = "1234";
+  private static final String GUEST = "guest";
+  private static final String ADMIN = "admin";
+
+
   @Bean
   public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-    var user = User.withUsername("guest").password(passwordEncoder.encode("1234")).roles("USER").build();
+    var user = User.withUsername(GUEST).password(passwordEncoder.encode(PASSWORD)).roles(GUEST).build();
 
-    var admin = User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("ADMIN").build();
+    var admin = User.withUsername(ADMIN).password(passwordEncoder.encode(PASSWORD)).roles(ADMIN).build();
 
     return new InMemoryUserDetailsManager(user, admin);
+  }
+
+  @Bean
+  SecurityFilterChain securiyFilterChain(HttpSecurity http) throws Exception {
+    return http.authorizeHttpRequests(
+        auth -> auth
+            .requestMatchers(WHITE_LIST)
+            .permitAll()
+            .anyRequest().authenticated())
+        .httpBasic(Customizer.withDefaults())
+        .build();
   }
 
   @Bean
